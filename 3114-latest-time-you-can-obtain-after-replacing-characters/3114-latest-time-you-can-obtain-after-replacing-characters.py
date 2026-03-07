194a9550348cb7f228ad8be3962f526c30749c53
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-03-07 15:37:49 +0530

    Time: 12 ms (86.67%), Space: 12.4 MB (40%) - LeetHub

--- 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.py
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.py
@@ -9,19 +9,19 @@ class Solution(object):
             if s[i] == "?":
                 if i == 0:
                     if s[1] == "?":
-                        answer[i] = 1
+                        answer[i] = "1"
                     elif int(s[1]) == 0 or int(s[1]) == 1: 
-                        answer[i] = 1
+                        answer[i] = "1"
                     
                     else:
-                        answer[i] = 0
+                        answer[i] = "0"
                 elif i == 1:
-                    if s[i-1] == 0:
-                        answer[i] = 9
+                    if s[i-1] == "0":
+                        answer[i] = "9"
                     else:
-                        answer[i] = 1
+                        answer[i] = "1"
                 elif i == 3:
-                    answer[i] = 5
+                    answer[i] = "5"
                 else:
                     answer[i] = "9"
         
