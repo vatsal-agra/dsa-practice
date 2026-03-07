a4d6d2b56b40bbf16b442ca5cc28f69d41ff6dad
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-03-07 15:36:20 +0530

    Time: 19 ms (46.67%), Space: 12.4 MB (40%) - LeetHub

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findLatestTime(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        answer = [i for i in s]
+        for i in range(len(s)):
+            if s[i] == "?":
+                if i == 0:
+                    if s[1] == "?":
+                        answer[i] = "1"
+                    elif int(s[1]) == 0 or int(s[1]) == 1: 
+                        answer[i] = "1"
+                    
+                    else:
+                        answer[i] = "0"
+                elif i == 1:
+                    if s[i-1] == "0":
+                        answer[i] = "9"
+                    else:
+                        answer[i] = "1"
+                elif i == 3:
+                    answer[i] = "5"
+                else:
+                    answer[i] = "9"
+        
+        return "".join(answer)
