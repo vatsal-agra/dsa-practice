e72404cc199d4f673ebeec126dba8be44faca141
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-03-10 11:55:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.py
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.py
@@ -1,24 +1,18 @@
 class Solution(object):
     def numberOfPairs(self, nums1, nums2, k):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :type k: int
-        :rtype: int
-        """
-        from collections import Counter
-
-        freq = Counter(nums2)
+        freq = {}
+        for num in nums2:
+            freq[num] = freq.get(num, 0) + 1
+        
         count = 0
-
         for x in nums1:
             if x % k != 0:
                 continue
             target = x // k
-            i=1
-            while i*i <= target:
+            i = 1
+            while i * i <= target:
                 if target % i == 0:
-                    count += freq.get(i,0)
+                    count += freq.get(i, 0)
                     if i != target // i:
                         count += freq.get(target // i, 0)
                 i += 1
