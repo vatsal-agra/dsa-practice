50d16bf072f1acd39dded7ad9521175fc999d399
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-03-10 12:29:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: bool
-        """
+        
         count = k
         for i in range(len(nums)):
             if nums[i] == 1:
@@ -13,4 +13,21 @@ class Solution(object):
                 count = 0
             else:
                 count+=1
+        return True"""
+
+        distance = 0
+        found = False
+        
+        for num in nums:
+            if num == 1:
+                
+                if distance <= k and found:
+                    return False 
+                
+                found = True
+
+                distance = 0
+            
+            distance += 1
+
         return True
