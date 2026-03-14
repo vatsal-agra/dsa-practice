ff118ca434685d248cccbc93b43d2c3d1d289a62
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-03-14 22:35:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         count = 0
         for i in range(len(nums)-1):
-            print(sum(nums[:i+1]) - sum(nums[i+1:]))
             if (sum(nums[:i+1]) - sum(nums[i+1:]))%2 == 0:
                 count += 1
         return count
