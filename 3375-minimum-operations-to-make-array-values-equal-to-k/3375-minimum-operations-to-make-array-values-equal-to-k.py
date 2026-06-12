159b4ac123fa5bd61b95747c14d3575786ff011e
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-06-12 23:20:27 +0530

    Time: 51 ms (33.33%), Space: 12.5 MB (33.33%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
@@ -9,10 +9,11 @@ class Solution(object):
             return -1
 
         else:
+            check = len(nums)-1
             count = 0
             nums.sort()
-            for i in range(len(nums)-1):
-                if nums[(len(nums)-1) - (i+1)] < nums[(len(nums)-1)-i]:
+            for i in range(check):
+                if nums[check - (i+1)] < nums[check-i]:
                     count = count + 1
 
             return count if k==min(nums) else count+1
