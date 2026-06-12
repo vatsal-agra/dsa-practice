becd56880af5f0d83b7d604657f8493aa91fdeef
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-06-12 23:37:48 +0530

    Time: 47 ms (61.11%), Space: 12.3 MB (72.22%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
@@ -5,12 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        if k > min(nums):
-            return -1
-
-        else:
-            check = []
-            for i in nums:
-                if i not in check:
-                    check.append(i)
-            return len(check)-1 if min(nums) == k else len(check) 
+        st = set()
+        for x in nums:
+            if x < k:
+                return -1
+            elif x > k:
+                st.add(x)
+        return len(st)
