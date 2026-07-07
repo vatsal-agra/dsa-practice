c7f0d9c238c47508b629456f589d81a14834278d
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-08 00:21:32 +0530

    Time: 154 ms (8.57%), Space: 12.9 MB (10%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
@@ -4,13 +4,20 @@ class Solution(object):
         :type rectangles: List[List[int]]
         :rtype: int
         """
-        greatest = 0
+        '''greatest = 0
         for i in rectangles:
             if min(i) > greatest:
                 greatest = min(i)
-        count = 0
+        
         for i in rectangles:
             if min(i) == greatest:
+                count = count + 1
+
+        return count'''
+        count = 0
+        rectangles.sort()
+        for i in rectangles:
+            if min(i) == min(rectangles[-1]):
                 count = count + 1
 
         return count
