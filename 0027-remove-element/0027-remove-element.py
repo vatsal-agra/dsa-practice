4ea23b0dedb18e0fe47098460fe96891167da6cc
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-07 23:55:35 +0530

    Time: 230 ms (1.29%), Space: 12.5 MB (16.19%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k=0
+        for i in range(len(nums)):
+            print(nums)
+            if nums[i] != val:
+                k=k+1
+            else:
+                nums[i] = 51
+        nums.sort()
+        return k
