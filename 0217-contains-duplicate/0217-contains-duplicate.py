b42828fbf927e9ea1c92f091ebf3922b5dc25268
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-10 15:21:56 +0530

    Time: 16 ms (74.74%), Space: 25.9 MB (30.85%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,10 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        check = {}
-        for i in nums:
-            if i not in check:
-                check[i] = 1
-            else:
-                return True
-        return False
+        return len(nums) != len(set(nums))
