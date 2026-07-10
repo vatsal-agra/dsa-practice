dd2303759671caf6b57016b05c3100c129d3e67a
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-10 22:51:45 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.11%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def areSimilar(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: bool
+        """
+        count = 0
+        num = len(mat[0])
+        check = []
+        shift = k%num if k>num else k
+        if shift == 0:
+            return True
+        for i in mat:
+            if count % 2 == 0:
+                check.append(i[shift:]+i[:shift])
+                
+            else:
+                check.append(i[num-shift:]+i[:num-shift])
+                
+            count += 1
+       
+        if check == mat:
+            return True
+        return False
