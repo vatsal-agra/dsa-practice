84443815c4e8c43318d85b32f6b1cc5b0581f675
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-13 00:16:35 +0530

    Time: 84 ms (5.06%), Space: 13.4 MB (93.39%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -9,7 +9,7 @@ class Solution(object):
         for i in nums:
             if i == 1:
                 count += 1
-                ans = max(ans, count)   # check the max right here, every step
+                ans = max(ans, count)  
             else:
                 count = 0
         return ans
