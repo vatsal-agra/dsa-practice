b0cd5eff783435b15d1f5b6c5289895685abbc14
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-13 00:45:30 +0530

    Time: 0 ms (100%), Space: 12.6 MB (59.64%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -5,13 +5,10 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        ans=[]
-        for i in nums1:
-            j = nums2.index(i)
-            for x in nums2[j+1:]:
-                if x > i:
-                    ans.append(x)
-                    break
-            else:                 
-                    ans.append(-1)
-        return ans
+        nxt = {}                              # ← the hashmap
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                nxt[stack.pop()] = num        # ← writing to it
+            stack.append(num)
+        return [nxt.get(x, -1) for x in nums1] 
