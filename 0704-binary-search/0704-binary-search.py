0a2253f968134fc315176db888b3ceaff2e0e047
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-13 22:02:48 +0530

    Time: 0 ms (100%), Space: 13.3 MB (70.23%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        s = 0
+        e = len(nums)-1
+        while s<=e:
+            mid = (s+e)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                e = mid-1
+            else:
+                s = mid+1
+        return -1
