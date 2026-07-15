128ba66c5179f370a783a231bcbe7883b90ed1a5
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-15 22:21:15 +0530

    Time: 2662 ms (7.19%), Space: 12.8 MB (94.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        ans = []
+        sum = 0
+        a = 0
+        b = len(nums)-1
+        for i in range(len(nums)-1):
+            while a < b:
+                if nums[a]+nums[b] == target:
+                    ans.append(a)
+                    ans.append(b)
+                a += 1
+            b -= 1
+            a = 0
+        return ans
