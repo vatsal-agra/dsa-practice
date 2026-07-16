2a641f905994ba9de2ede2648ff95a232d7a9fbf
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-16 22:22:15 +0530

    Time: 0 ms (100%), Space: 12.3 MB (52.47%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1:
+            return True
+        check = []
+        seen = False
+
+        while not seen:
+            a = n
+            sum = 0
+            while a>0:
+                digit = a%10
+                sum += digit*digit
+                a = a // 10
+            n = sum
+            if sum == 1:
+                return True
+            if sum in check:
+                seen = True
+            else:
+                check.append(sum)
+        return False
