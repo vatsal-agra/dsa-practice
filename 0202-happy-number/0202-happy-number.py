86b9fcf2e1e08eb233be681abbae9567852f3f1b
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-16 22:25:57 +0530

    Time: 0 ms (100%), Space: 12.3 MB (88.77%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,26 +1,22 @@
 class Solution(object):
     def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        if n == 1:
-            return True
-        check = {}
-        seen = False
+        def next_num(x):
+            total = 0
+            while x:
+                digit = x % 10
+                total += digit * digit
+                x //= 10
+            return total
 
-        while not seen:
-            a = n
-            sum = 0
-            while a>0:
-                digit = a%10
-                sum += digit*digit
-                a = a // 10
-            n = sum
-            if sum == 1:
+        slow = n
+        fast = n
+
+        while True:
+            slow = next_num(slow)
+            fast = next_num(next_num(fast))
+
+            if fast == 1:
                 return True
-            if sum in check:
-                seen = True
-            else:
-                check[sum] = 1
-        return False
+
+            if slow == fast:
+                return False
