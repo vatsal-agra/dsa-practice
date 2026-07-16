486285c77b1d2a3221a0ec8afc2a6145224ff594
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-07-16 23:56:53 +0530

    Time: 0 ms (100%), Space: 19.2 MB (71.2%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -1,22 +1,23 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        n = len(s)
-        ans = [n] * n
+        def update_index(value):
+            while value < len(s) and s[value] != c:
+                value += 1
+            return value
 
-        prev = -n
+        ans = []
 
-        # Left to right
-        for i in range(n):
-            if s[i] == c:
-                prev = i
-            ans[i] = i - prev
+        prev_index = -float('inf')      # No previous occurrence yet
+        next_index = update_index(0)    # First occurrence of c
 
-        prev = 2 * n
+        for i in range(len(s)):
+            if i == next_index:
+                prev_index = next_index
+                next_index = update_index(next_index + 1)
 
-        # Right to left
-        for i in range(n - 1, -1, -1):
-            if s[i] == c:
-                prev = i
-            ans[i] = min(ans[i], prev - i)
+            left = i - prev_index
+            right = next_index - i if next_index < len(s) else float('inf')
+
+            ans.append(min(left, right))
 
         return ans
