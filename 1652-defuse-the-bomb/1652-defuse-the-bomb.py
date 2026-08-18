060a6bfc97c1371200a3d9d71dad23777ee506ee
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-08-18 23:47:55 +0530

    Time: 7 ms (31.8%), Space: 19.3 MB (35.74%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,24 +1,21 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        window = [0]*len(code)
         check = len(code)
+        window = [0]*check
+        
         if k>0:
-            for i in range(len(code)):
+            for i in range(check):
                 counter = i+1
                 while counter <= i+k:
-                    #print(counter, i ,k)
                     window[i] += code[counter % check]
                     counter += 1
-                    #print(window)
             return window
         elif k<0:
-            for i in range(len(code)):
+            for i in range(check):
                 counter = i-1
                 while counter >= i+k:
-                    #print(counter, i ,k)
                     window[i] += code[counter % check]
                     counter -= 1
-                    #print(window)
             return window
         else:
             return window
