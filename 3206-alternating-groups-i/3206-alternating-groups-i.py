5599efc7d6ffeacb3820c0d11e6edbfc03ba3839
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-08-19 13:51:00 +0530

    Time: 59 ms (64.15%), Space: 19 MB (98.64%) - LeetHub

--- 3206-alternating-groups-i/3206-alternating-groups-i.py
+++ 3206-alternating-groups-i/3206-alternating-groups-i.py
@@ -1,19 +1,9 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        n = len(colors)
         count = 0
-        k = len(colors)
-        for i in range(k):
-            a=i
-            b=i+3
-            if b > k:
-                b = b%k
-                check = colors[a:] + colors[:b]
-            else:
-                check = colors[a:b]
-            met = False
-            for j in range(2):
-                if check[j] == check[j+1]:
-                    met = True
-            if met == False:
+        for i in range(n):
+            # three consecutive tiles, wrapping with % n
+            if colors[i] != colors[(i+1) % n] and colors[(i+1) % n] != colors[(i+2) % n]:
                 count += 1
         return count
