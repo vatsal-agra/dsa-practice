4472327f3a0e2fcae7e5de7df19d0855d0cc4de0
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-08-23 23:12:26 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.05%) - LeetHub

--- 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.py
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.py
@@ -2,11 +2,5 @@ class Solution:
     def rearrangeString(self, s: str, x: str, y: str) -> str:
         ans = []
         count = 0
-        for i in s:
-            if i == x:
-                count += 1
-            else:
-                ans.append(i)
-        for i in range(count):
-            ans.append(x)
+        ans = [c for c in s if c != x] + [x] * s.count(x)
         return "".join(ans)
