c136e65024cf4819ba937f5e1d44b5214d516d38
Vatsal Agrawal <agrawal.vatsal@gmail.com>
2026-08-26 01:00:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (80.16%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for i in logs:
+            if i == "../":
+                if stack:
+                    stack.pop()
+                else:
+                    continue
+            elif i == "./":
+                continue
+            else:
+                stack.append(i)
+        return len(stack)
